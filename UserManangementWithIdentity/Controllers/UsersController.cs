a40751c46b2cdca590ab32867c632593c9108774
mohamed kamal <mkahf5103@gmail.com>
2022-06-26 19:29:23 +0400

    add user controller

---
+++ UserManangementWithIdentity/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using UserManangementWithIdentity.Models;
+using UserManangementWithIdentity.ViewModels;
+
+namespace UserManangementWithIdentity.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UsersController(UserManager<ApplicationUser>userManager,RoleManager<IdentityRole>roleManager)
+        {
+           _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        public async Task <IActionResult> Index()
+        {
+            var users =await _userManager.Users.Select(user => new UserViewModel
+            {
+                Id=user.Id,
+                FirstNmae=user.FirstName,
+                LastName=user.LastName,
+                UserName=user.UserName,
+                Email=user.Email,
+                Roles=_userManager.GetRolesAsync(user).Result
+            }).ToListAsync();
+
+            return View(users);
+        }
+    }
+}
---
+++ UserManangementWithIdentity/ViewModels/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace UserManangementWithIdentity.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+        public string FirstNmae { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+    }
+}
