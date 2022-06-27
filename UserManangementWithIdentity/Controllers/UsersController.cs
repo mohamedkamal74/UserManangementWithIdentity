87adc3cf057482e6e048bddca1c500e2e6df827f
mohamed kamal <mkahf5103@gmail.com>
2022-06-27 20:38:44 +0400

    add manage roles action and view

--- UserManangementWithIdentity/Controllers/UsersController.cs
+++ UserManangementWithIdentity/Controllers/UsersController.cs
@@ -9,30 +9,50 @@ using UserManangementWithIdentity.ViewModels;
 
 namespace UserManangementWithIdentity.Controllers
 {
-    [Authorize(Roles ="Admin")]
+    [Authorize(Roles = "Admin")]
     public class UsersController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
 
-        public UsersController(UserManager<ApplicationUser>userManager,RoleManager<IdentityRole>roleManager)
+        public UsersController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
-           _userManager = userManager;
+            _userManager = userManager;
             _roleManager = roleManager;
         }
-        public async Task <IActionResult> Index()
+        public async Task<IActionResult> Index()
         {
-            var users =await _userManager.Users.Select(user => new UserViewModel
+            var users = await _userManager.Users.Select(user => new UserViewModel
             {
-                Id=user.Id,
-                FirstNmae=user.FirstName,
-                LastName=user.LastName,
-                UserName=user.UserName,
-                Email=user.Email,
-                Roles=_userManager.GetRolesAsync(user).Result
+                Id = user.Id,
+                FirstNmae = user.FirstName,
+                LastName = user.LastName,
+                UserName = user.UserName,
+                Email = user.Email,
+                Roles = _userManager.GetRolesAsync(user).Result
             }).ToListAsync();
 
             return View(users);
+        }
+
+        public async Task<IActionResult> ManageRoles(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+                return NotFound();
+            var roles = await _roleManager.Roles.ToListAsync();
+            var viewmodel = new UserRolesViewModel
+            {
+                UserId = user.Id,
+                UserName = user.UserName,
+                Roles = roles.Select(role => new RoleViewModel
+                {
+                    RoleId = role.Id,
+                    RoleName = role.Name,
+                    IsSelected = _userManager.IsInRoleAsync(user, role.Name).Result
+                }).ToList()
+            };
+            return View(viewmodel);
         }
     }
 }
