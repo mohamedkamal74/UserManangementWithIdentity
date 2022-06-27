8f0a955ea06a381777cd3c9344b69c6d98123ff6
mohamed kamal <mkahf5103@gmail.com>
2022-06-27 22:47:54 +0400

    Add new user by admin action and view

--- UserManangementWithIdentity/Controllers/UsersController.cs
+++ UserManangementWithIdentity/Controllers/UsersController.cs
@@ -35,6 +35,61 @@ namespace UserManangementWithIdentity.Controllers
             return View(users);
         }
 
+        public async Task<IActionResult> Add()
+        {
+            var roles = await _roleManager.Roles.Select(r => new RoleViewModel
+            {
+                RoleId = r.Id,
+                RoleName = r.Name,
+            }).ToListAsync();
+            var viewmodel = new AddUserViewModel
+            {
+                Roles = roles
+            };
+            return View(viewmodel);
+        }
+
+        public async Task<IActionResult> Add(AddUserViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            if(!model.Roles.Any(r => r.IsSelected))
+            {
+                ModelState.AddModelError("Roles", "Please choose at least one role");
+                return View(model);
+            }
+            if(await _userManager.FindByEmailAsync(model.Email) != null)
+            {
+                ModelState.AddModelError("Email", "Email is already Exist");
+                return View(model);
+            }
+            if (await _userManager.FindByEmailAsync(model.UserName) != null)
+            {
+                ModelState.AddModelError("UserName", "UserName is already Exist");
+                return View(model);
+            }
+
+            var user = new ApplicationUser
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                FirstName = model.FirstName,
+                LastName = model.LastName
+            };
+         var result=await _userManager.CreateAsync(user,model.Password);
+            if (!result.Succeeded)
+            {
+                foreach (var Error in result.Errors)
+                {
+                    ModelState.AddModelError("Roles", Error.Description);
+                }
+            }
+            await _userManager.AddToRolesAsync(user, model.Roles.Where(r => r.IsSelected).Select(r => r.RoleName));
+            return RedirectToAction(nameof(Index));
+        }
+
         public async Task<IActionResult> ManageRoles(string id)
         {
             var user = await _userManager.FindByIdAsync(id);
@@ -54,5 +109,25 @@ namespace UserManangementWithIdentity.Controllers
             };
             return View(viewmodel);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ManageRoles(UserRolesViewModel model)
+        {
+            var user = await _userManager.FindByIdAsync(model.UserId);
+            if (user == null)
+                return NotFound();
+            var userRoles = await _userManager.GetRolesAsync(user);
+            foreach (var role in model.Roles)
+            {
+                if (userRoles.Any(r => r == role.RoleName) && !role.IsSelected)
+                    await _userManager.RemoveFromRoleAsync(user, role.RoleName);
+                if (!userRoles.Any(r => r == role.RoleName) && role.IsSelected)
+                    await _userManager.AddToRoleAsync(user, role.RoleName);
+            }
+            return RedirectToAction(nameof(Index));
+
+        }
+
     }
 }
